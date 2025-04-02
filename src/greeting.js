885196ce735249eb6a69844b07040cb73cee68ca
dipak1920basnet <dipak1920basnet@gmail.com>
2025-04-02 10:49:57 +0545

    adds form to a page

--- src/greeting.js
+++ src/greeting.js
@@ -1,6 +1,7 @@
 function navigate(element)
 {
     const button = document.createElement('button')
+    button.setAttribute('id',`${element}`)
     button.textContent = element;
 
 
@@ -15,6 +16,65 @@ function navigate(element)
     }
 }
 
+// Make a reservation
+function make_reservation(){
+    let form = document.createElement('form');
+    form.setAttribute('class','row g-3');
+    form.innerHTML = `
+        <div class="row g-3">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+            </div>
+        </div>
+        <div class="col-md-6">
+          <label for="inputEmail4" class="form-label">Email</label>
+          <input type="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-6">
+          <label for="inputPassword4" class="form-label">Password</label>
+          <input type="password" class="form-control" id="inputPassword4">
+        </div>
+        <div class="col-12">
+          <label for="inputAddress" class="form-label">Address</label>
+          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        </div>
+        <div class="col-12">
+          <label for="inputAddress2" class="form-label">Address 2</label>
+          <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+        </div>
+        <div class="col-md-6">
+          <label for="inputCity" class="form-label">City</label>
+          <input type="text" class="form-control" id="inputCity">
+        </div>
+        <div class="col-md-4">
+          <label for="inputState" class="form-label">State</label>
+          <select id="inputState" class="form-select">
+            <option selected>Choose...</option>
+            <option>...</option>
+          </select>
+        </div>
+        <div class="col-md-2">
+          <label for="inputZip" class="form-label">Zip</label>
+          <input type="text" class="form-control" id="inputZip">
+        </div>
+        <div class="col-12">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck">
+            <label class="form-check-label" for="gridCheck">
+              Check me out
+            </label>
+          </div>
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    `;
+    return form;
+}
+
 // Added a page content 
 function content(page_content)
 {
@@ -27,10 +87,16 @@ function content(page_content)
         const title = document.createElement('h2')
         title.innerText = titles[i]
         new_content.appendChild(title)
-        const contents = document.createElement('p')
-        contents.innerText = page_content[titles[i]]
-        new_content.appendChild(contents)
+        if (titles[i] ===  "reservation")
+        {
+            new_content.appendChild(page_content[titles[i]]())
+        }
+        else{
+            const contents = document.createElement('p')
+            contents.innerText = page_content[titles[i]]
+            new_content.appendChild(contents)
+        }
         content_element.appendChild(new_content)
     }
 }
-export {navigate,content};
+export {navigate,content, make_reservation};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css"
-import {navigate,content,create_function_list} from "./greeting.js";
+import {navigate,content,make_reservation} from "./greeting.js";
 
 // import {videoSrc} from "./assets/video.mp4"
 
@@ -7,7 +7,7 @@ import {navigate,content,create_function_list} from "./greeting.js";
 
 const nav_key_content = {"Home":"Welcome to Savory Haven, a warm and inviting family-owned restaurant where every dish is crafted with passion and care. Located in the heart of the city, we take pride in serving fresh, flavorful meals made from locally sourced ingredients. Our menu offers a delightful mix of classic comfort foods and creative new flavors, ensuring there's something for everyone—from hearty breakfasts to gourmet lunches and indulgent dinners. Whether you're stopping by for a quick bite, a leisurely meal with loved ones, or a special celebration, our cozy atmosphere and friendly staff make you feel right at home. With a rustic yet modern décor, handcrafted beverages, and a commitment to quality, Savory Haven is more than just a place to eat—it’s an experience to savor. Come join us and let us treat you to a meal you won’t forget!",
     "Menu":["Appetizers Main Course Dessert bevrages"],
-    "reservation":null}
+    "reservation":make_reservation}
 const nav_bar = Object.keys(nav_key_content);
 for (let i = 0; i < nav_bar.length; i++)
 {
